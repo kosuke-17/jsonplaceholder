c8cde87d0739e63524cc7410d73b004c158922f2
kosuke-17 <tamutamu17@icloud.com>
2021-05-15 10:12:27 +0900

    Buttonコンポーネントの作成

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Button() {
+const Button = ({onClick, color, text})=> {
   return (
     <div>
-      
+      <button onClick={onClick} className={`ui ${color} button`}>{text}</button>
     </div>
   )
 }
 
-export default Button
+export default Button;
